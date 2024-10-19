f20ebe8d0a2e225c417aa0103c35cd252efb5f59
C1ph3r Phr3ak <C1ph3rPhr3ak@proton.me>
2024-10-19 13:13:15 -0700

    Update install_dependncies.bat

--- install_dependncies.bat
+++ install_dependncies.bat
@@ -1,17 +1,18 @@
-#-----------------------------------------------
-#C1ph3rPhr3ak Non-Redistributable License v1.0
+# -----------------------------------------------
 
-#Copyright (c) 2024 C1ph3rPhr3ak. All rights reserved.
+# C1ph3rPhr3ak Non-Redistributable License v1.0
 
-#This software is the confidential and proprietary information of C1ph3rPhr3ak. You shall not disclose, distribute, or reproduce such Confidential Information and shall use it only in accordance with the terms of this license.
+# Copyright (c) 2024 C1ph3rPhr3ak. All rights reserved.
 
-#DISCLAIMER:
-#This software is provided "AS IS," without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event shall the author or copyright holder be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
+# This software is the confidential and proprietary information of C1ph3rPhr3ak. You shall not disclose, distribute, or reproduce such Confidential Information and shall use it only in accordance with the terms of this license.
 
-#The author is not liable for any legal actions, damages, or other consequences resulting from the misuse of this software, including but not limited to illegal or malicious use.
+# DISCLAIMER:
+# This software is provided "AS IS," without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event shall the author or copyright holder be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
 
-#For further inquiries, contact C1ph3rPhr3ak at C1ph3rPhr3ak@proton.me
-#------------------------------------------------
+# The author is not liable for any legal actions, damages, or other consequences resulting from the misuse of this software, including but not limited to illegal or malicious use.
+
+# For further inquiries, contact C1ph3rPhr3ak at C1ph3rPhr3ak@proton.me
+# ------------------------------------------------
 
 @echo off
 
